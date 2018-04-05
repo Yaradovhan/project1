b3c832712df3694dd4d1b3ce6d1be6878bb9bca3
Yaradovhan <yara.dovhan@gmail.com>
2018-04-05 21:03:12 +0300

    add new db

--- testdb.sql
+++ testdb.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.9
+-- version 4.7.7
 -- https://www.phpmyadmin.net/
 --
--- Хост: localhost
--- Время создания: Апр 03 2018 г., 21:21
--- Версия сервера: 10.1.31-MariaDB
--- Версия PHP: 7.2.3
+-- Хост: 127.0.0.1:3306
+-- Время создания: Апр 05 2018 г., 21:02
+-- Версия сервера: 5.6.38
+-- Версия PHP: 7.0.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,6 +25,24 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Структура таблицы `admin`
+--
+
+CREATE TABLE `admin` (
+  `name` varchar(50) NOT NULL,
+  `pass` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `admin`
+--
+
+INSERT INTO `admin` (`name`, `pass`) VALUES
+('admin', '123');
+
+-- --------------------------------------------------------
+
+--
 -- Структура таблицы `tasks`
 --
 
@@ -40,9 +58,8 @@ CREATE TABLE `tasks` (
 --
 
 INSERT INTO `tasks` (`id`, `text`, `img`, `date`) VALUES
-(14, 'iocdjoiveovbeopawgph', 'shshsrthrsth', '2018-04-03 12:44:17'),
-(19, 'jetklsajw57ksrjr', 'r7eksrjryk', '2018-04-03 12:44:17'),
-(22, 'EHSRJ7KES6USR6I', 'SRTRJDSDSWRE5UWR', '2018-04-03 12:44:17');
+(78, 'yarabest', 'view/assets/img/', '2018-04-05 16:52:19'),
+(79, 'QAdsfdgfhgh', 'view/assets/img/', '2018-04-05 16:52:36');
 
 -- --------------------------------------------------------
 
@@ -62,10 +79,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `pass`, `email`) VALUES
-(1, 'Admin', 0, ''),
-(3, 'user2', NULL, 'user2@user2.com'),
-(7, 'user16', NULL, 'user@user.com'),
-(10, 'user266', NULL, 'user2@user2.com');
+(42, 'yarabest', NULL, 'yarabest'),
+(43, '1234567', NULL, 'wertyu6789');
 
 -- --------------------------------------------------------
 
@@ -83,9 +98,8 @@ CREATE TABLE `users_tasks` (
 --
 
 INSERT INTO `users_tasks` (`user_id`, `task_id`) VALUES
-(1, 22),
-(3, 14),
-(7, 19);
+(42, 78),
+(43, 79);
 
 --
 -- Индексы сохранённых таблиц
@@ -118,13 +132,13 @@ ALTER TABLE `users_tasks`
 -- AUTO_INCREMENT для таблицы `tasks`
 --
 ALTER TABLE `tasks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=80;
 
 --
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
